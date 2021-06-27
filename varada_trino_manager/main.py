706df97af59ac7ebaa4ec592eb4ac03627b8aab9
hadas <hadas@varada.io>
2021-06-27 16:57:04 +0300

    add rules apply,get,delete

--- varada_trino_manager/main.py
+++ varada_trino_manager/main.py
@@ -1,6 +1,7 @@
 from json import dumps
 from typing import Tuple
 from .constants import Paths
+from json import dumps
 from .configuration import get_config
 from .rest_commands import RestCommands
 from .connections import PrestoRest, Trino
@@ -8,7 +9,8 @@ from .run_queries import run as query_runner
 from logging import INFO, DEBUG, StreamHandler
 from .warm_validate import run as warm_validate
 from .query_json_jstack import run as query_json_jstack
-from click import group, argument, option, echo, Path as ClickPath
+from .rules import delete as delete_rule, get as get_rule, apply as apply_rule
+from click import group, argument, option, echo, Path as ClickPath, exceptions
 from .utils import read_file_as_json, logger, session_props_to_dict
 from .remote import (
     parallel_download,
@@ -203,36 +205,73 @@ def rules():
     """
 
 
+@option("-c", "--csv-path", type=ClickPath(exists=True), help="""Full path and name of CSV file with rule(s) to set.
+CSV format as per the below example:
+
+\b
+# Headers row, then row per rule:
+schema,table,colNameId,colWarmUpType,priority,ttl,predicates
+default,trips_partitions_table,tripid,COL_WARM_UP_TYPE_BASIC,8,PT720H,"type:PartitionValue,columnId:d_date,value:2018-01-02","type:PartitionValue,columnId:d_date,value:2018-01-03"
+default,trips_date_int_date_table,char_10,COL_WARM_UP_TYPE_DATA,7,PT50M,"type:DateRangeSlidingWindow,columnId:date_date,windowDateFormat:yyyy-MM-dd,startRangeDaysBefore:450,endRangeDaysBefore:448"
+...
+\b
+Note that predicate sets are double quoted
+For WarmUpType/predicate examples and more info per CSV column see https://docs.varada.io/docs/acceleration-instruction-commands
+""")
+@option("-j", "--json-path", type=ClickPath(exists=True), help="""Location of JSON with rule(s) to set.
+JSON format as per the below basic example:
+
+\b
+{
+    "schema": "SCHEMA",
+    "table": "TABLE",
+    "colNameId": "COLUMN_NAME",
+    "colWarmUpType": "COL_WARM_UP_TYPE_BASIC",
+    "priority": 8,
+    "ttl": "PT0M",
+    "predicates": []
+}
+\b
+
+For predicate examples and more info per key see https://docs.varada.io/docs/acceleration-instruction-commands
+""")
 @rules.command()
-def generate():
+def apply(json_path, csv_path):
     """
-    Generate rule
+    Apply rules to the cluster from json or csv file
     """
-    pass
+    con = get_config().get_connection_by_name("coordinator")
+    apply_rule(con=con, json_path=json_path, csv_path=csv_path)
 
 
+@option("-t", "--table", type=str, required=False, help='Get user rules associated with table')
+@option("-c", "--column", type=str, required=False, help='Get user rules associated with column. Must specify table as well')
+@option("-d", "--destination-dir", type=ClickPath(), help="Save rules to destination dir as json file")
 @rules.command()
-def apply():
+def get(table, column, destination_dir):
     """
-    Apply rule to the cluster
+    Get rules from the Varada cluster, by default retrieve all
     """
-    pass
+    con = get_config().get_connection_by_name("coordinator")
+    get_rule(con=con, table=table, column=column, destination_dir=destination_dir)
 
 
+@option("-a", "--all-rules", is_flag=True, default=False, help="Delete all user rules from the Varada cluster. It is recommended to backup the rules first by running: vtm rules get -a")
+@option("-i", "--rule-ids", type=str, default=None, help='ID of rule to be deleted, if multiple - comma separated. Example: vtm -v rules delete -i 1106600307,1830309151')
 @rules.command()
-def get():
-    """
-    Get rule from the cluster
-    """
-    pass
-
-
-@rules.command()
-def delete():
+def delete(rule_ids, all_rules):
     """
     Delete rule from the cluster
     """
-    pass
+    con = get_config().get_connection_by_name("coordinator")
+    if (rule_ids is None) and not all_rules:
+        logger.info('Either -a or -i option is required')
+        raise exceptions.Exit(code=1)
+    proceed = input(f'Delete rules: {rule_ids if rule_ids else "all rules"} from the cluster? [y/N]: ')
+    if proceed == 'n':
+        logger.info('Aborting, no rules will be deleted')
+        raise exceptions.Exit(code=1)
+    delete_rule(con=con, rule_ids=rule_ids, all_rules=all_rules)
 
 
 @option(
---
+++ varada_trino_manager/rules.py
@@ -0,0 +1,85 @@
+from time import sleep
+from pathlib import Path
+from csv import DictReader
+from .utils import logger
+from json import dumps, load, dump
+from .connections import VaradaRest
+from click import exceptions
+from .configuration import Connection
+
+
+def apply(con: Connection, json_path: Path = None, csv_path: Path = None):
+    try:
+        if json_path:
+            fd = open(json_path)
+            rules_file = load(fd)
+        elif csv_path:
+            fd = open(csv_path, mode='r')
+            rules_file = DictReader(fd)
+        else:
+            logger.exception(f'No valid CSV or JSON to read from')
+            raise exceptions.Exit(code=1)
+
+        with VaradaRest(con=con) as varada_rest:
+            if rules_file:
+                for rule in rules_file:
+                    rule['priority'] = int(rule['priority'])
+                    if rule['predicates']:
+                        # Convert predicates to list of dict(s), cleanup None str (multi predicates), convert to int if needed
+                        if 'DateRangeSlidingWindow' in rule['predicates']:
+                            rule['predicates'] = [{k: v for k, v in [pair.split(':') for pair in rule['predicates'].split(',')]}]
+                            rule['predicates'][0]['startRangeDaysBefore'] = int(rule['predicates'][0]['startRangeDaysBefore'])
+                            rule['predicates'][0]['endRangeDaysBefore'] = int(rule['predicates'][0]['endRangeDaysBefore'])
+                        elif 'PartitionValue' in rule['predicates']:
+                            if None in rule.keys():
+                                # more than one predicate
+                                rule[None].insert(0, rule['predicates'])
+                                rule['predicates'] = rule[None]
+                                del rule[None]
+                                rule['predicates'] = [{k: v for k, v in [pair.split(':') for pair in predicate.split(',')]} for predicate in rule['predicates']]
+                            else:
+                                rule['predicates'] = [{k: v for k, v in [pair.split(':') for pair in rule['predicates'].split(',')]}]
+                    logger.info(f'Setting rule: {rule}')
+                    varada_rest.set_warmup_rule(json_data=rule)
+            elif rules_file:
+                for rule in rules_file:
+                    varada_rest.set_warmup_rule(json_data=rule)
+    finally:
+        fd.close()
+
+
+def get(con: Connection, table, column, destination_dir):
+    with VaradaRest(con=con) as varada_rest:
+        all_rules_str = varada_rest.get_warmup_rules()
+        if column:
+            if not table:
+                logger.info(f'Missing table for column {column}, please run with -t TABLE_NAME')
+                exit()
+            else:
+                logger.info(f'Getting rules for table {table}, column {column}')
+                rules_str = [rule for rule in all_rules_str if rule['table'] == table and rule['colNameId'] == column]
+        elif table:
+            logger.info(f'Rules for table {table}:')
+            rules_str = [rule for rule in all_rules_str if rule['table'] == table]
+        else:
+            rules_str = all_rules_str
+        if destination_dir:
+            logger.info(f'Saving rules to {destination_dir}/rules.json')
+            with open(f'{destination_dir}/rules.json', 'w') as fd:
+                dump(rules_str, fd, indent=4)
+        else:
+            logger.info(dumps(rules_str, indent=4))
+
+
+def delete(con: Connection, rule_ids: str = None, all_rules: bool = False):
+    """
+    Delete rule from the cluster
+    """
+    with VaradaRest(con=con) as varada_rest:
+        if rule_ids:
+            logger.info(f'Deleting rule(s): {rule_ids}')
+            [varada_rest.del_warmup_rule(int(rule_id)) for rule_id in rule_ids.split(',')]
+        elif all_rules:
+            logger.info('Deleting all rules from the cluster')
+            all_rules_str = varada_rest.get_warmup_rules()
+            [varada_rest.del_warmup_rule(int(rule['id'])) for rule in all_rules_str]
